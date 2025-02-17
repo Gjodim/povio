f06cbc9ed477c1ea1632225315a2b6dad4ac1e47
Gjorgi Dimov <gjorgi@foxstone.ch>
2025-02-17 02:09:23 +0100

    add in data/test-data again

---
+++ data/test-data.ts
@@ -0,0 +1,25 @@
+export const TestData = {
+    signInData: {
+      invalidUsername: "invalidSignIn",
+      invalidPassword: "testPassword1!",
+    },
+  
+    signUpData: {
+      Name: "John",
+      Email: "invalidSignUp",
+      Password: "testPassword1!",
+      UnmatchingPassword: "testPassword1!1@",
+    },
+  
+    campaignData: {
+      Name: "NameOfCampaign",
+      Description: "DescriptionOfCampaign",
+    },
+  
+    newUser: {
+      Name: "",
+      Email: "123",
+      Password: "",
+    },
+  };
+  
